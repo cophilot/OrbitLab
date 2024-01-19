bacf00bb6ae1a1d9a707bbc1f694da9e8b501d85
cophilot <philxsb@gmail.com>
2024-01-19 22:27:00 +0100

    update: Fri 19 Jan 2024 22:27:00 CET

--- src/app/coordinate-system/coordinate-system.component.ts
+++ src/app/coordinate-system/coordinate-system.component.ts
@@ -7,7 +7,32 @@ import { ObjectService } from '../service/object.service';
   styleUrls: ['./coordinate-system.component.sass'],
 })
 export class CoordinateSystemComponent {
-  constructor(private objectService: ObjectService) {}
+  xBars: number[] = [];
+  yBars: number[] = [];
+
+  constructor(private objectService: ObjectService) {
+    const gridGap = 50;
+
+    let middle = window.innerWidth / 2;
+    this.xBars.push(middle);
+
+    let offset = 0;
+    while (middle - offset > 0) {
+      offset += gridGap;
+      this.xBars.push(middle + offset);
+      this.xBars.push(middle - offset);
+    }
+
+    middle = window.innerHeight / 2;
+    this.yBars.push(middle);
+
+    offset = 0;
+    while (middle - offset > 0) {
+      offset += gridGap;
+      this.yBars.push(middle + offset);
+      this.yBars.push(middle - offset);
+    }
+  }
 
   unselectObject() {
     this.objectService.setSelectedObject(null);
